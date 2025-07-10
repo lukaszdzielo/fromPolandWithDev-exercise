359b9480b0a8f934df2369ea23e30be6b6e49458
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2025-07-10 17:53:20 +0200

    recommended section added

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,15 @@ const updateUrlParams = (name, value) => {
   const newUrl = `${window.location.pathname}?${params.toString()}`;
   window.history.replaceState({}, "", newUrl);
 };
+
+const scrollTo = (el) => {
+  el.scrollIntoView({ behavior: "smooth", block: "start" });
+};
+
+const formattedPrice = (price) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+  }).format(price);
+};
